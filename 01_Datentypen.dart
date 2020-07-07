76c3dded7081dce6a4863efe55fe9bea5f16fbdf
JustusEs <67897576+JustusEs@users.noreply.github.com>
2020-07-07 11:49:57 +0200

    Create 01_Datentypen.dart

---
+++ 01_Datentypen.dart
@@ -0,0 +1,13 @@
+main(){
+  int zahl = 3;
+  int zahl2= 4;
+  double kommazahl = 1.5;
+  double kommazahl2 = 2.5;
+  String name = "Justus";
+  
+  print(zahl);
+  print(zahl2);
+  print(kommazahl2);
+  print(kommazahl);
+  print(name);
+}
